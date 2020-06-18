3d7f81590e80c24cf359b5c6f025a36323cc4900
David Nunez <arizonatribe@gmail.com>
2020-06-18 00:59:45 -0700

    added a createTransitionHandler export (is there a better name for this?)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const createMachine = require('./createMachine')
+const createTransitionHandler = require('./transitions')
 const getNextState = require('./getNextState')
 
 module.exports = createMachine
 module.exports.getNextState = getNextState
+module.exports.createTransitionHandler = createTransitionHandler
